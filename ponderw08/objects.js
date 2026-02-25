135d2032ccff189191529a92a2f5007f765391c0
yarealexa <sal17018@byui.edu>
2026-02-25 16:48:58 -0700

    all assign

---
+++ ponderw08/objects.js
@@ -0,0 +1,65 @@
+
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'jslogo.png',
+    sections: [
+    { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+    ],
+    enrollStudent: function (sectionNum) {
+        // find the right section...Array.findIndex will work here
+        const sectionIndex = this.sections.findIndex(
+          (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+          this.sections[sectionIndex].enrolled++;
+          renderSections(this.sections);
+        }
+      }
+      
+  };
+
+
+
+console.log(aCourse.code);
+aCourse.code = 'WDD131';
+console.log(aCourse.code);
+
+document.querySelector('#courseName').textContent = aCourse.name;
+document.querySelector('#courseCode').textContent = aCourse.code;
+
+document.querySelector('img').setAttribute('src', aCourse.logo);
+document.querySelector('img').setAttribute('alt', aCourse.logo);
+document.querySelector('img').setAttribute('width', 200);
+
+console.log(aCourse.sections[1].instructor);
+console.log(aCourse.sections[0].roomNum);
+
+
+
+
+
+function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+const html = sections.map(sectionTemplate);
+document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
+
+
+            
